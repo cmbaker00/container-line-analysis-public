5a1e91de51f58f88572901dc86bde6a053c98336
cmbaker00 <cmbaker00@gmail.com>
2021-10-14 17:51:51 +1100

    verified that the new function to call stan fit works with all data and line-only data

--- simulation_study/simulation_stan_analysis/run_stan_fits_for_simulated_datasets.R
+++ simulation_study/simulation_stan_analysis/run_stan_fits_for_simulated_datasets.R
@@ -1,14 +1,15 @@
 
-source('simulations_study/simulation_stan_analysis/fit_simulated_data_full_model.R')
+source('simulation_study/simulation_stan_analysis/fit_simulated_data_full_model.R')
 
 
-data_filename <- 'full_data_2_5_corr_10000'
+data_filename <- 'line_data_2_5_no_corr_1000'
 load_sim_data <- read.csv(paste0('simulation_study/simulation_data/container_line_simulated_data_entrysize_2_5_entry_corr0.25_rows_1e+05.csv'))
-load_sim_data <- load_sim_data[1:10000,]
+load_sim_data <- load_sim_data %>% filter(Mode == 'Line')
+load_sim_data <- load_sim_data[1:1000,]
 #load_sim_data <- load_sim_data %>% mutate(Country=1)
 #load_sim_data <- load_sim_data %>% mutate(Entry=1)
 
 run_stan_simulated_data(load_sim_data,
                         data_filename,
-                        entry_correlation_flag = TRUE
+                        entry_correlation_flag = FALSE
 )
