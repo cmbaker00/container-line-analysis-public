e53f438e2d41d5ca1425be8b218008b551fc2f75
cmbaker00 <cmbaker00@gmail.com>
2021-10-22 15:40:00 +1100

    adding first set of stan simulation fits

--- simulation_study/simulation_stan_analysis/run_stan_fits_for_simulated_datasets.R
+++ simulation_study/simulation_stan_analysis/run_stan_fits_for_simulated_datasets.R
@@ -29,7 +29,8 @@ input_params_line_only_no_sd = data.frame(
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = c(1000, 2000, 3000, 4000, 5000),
-  num_container_data = rep(0, num_scenarios)
+  num_container_data = rep(0, num_scenarios),
+  entry_random_effect = rep(FALSE, num_scenarios)
 )
 
 loop_over_input_params_fit_stan(input_params_line_only_no_sd)
@@ -41,7 +42,8 @@ input_params_container_only_no_sd = data.frame(
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = rep(0, num_scenarios),
-  num_container_data = c(1000, 2000, 3000, 4000, 5000)
+  num_container_data = c(1000, 2000, 3000, 4000, 5000),
+  entry_random_effect = rep(FALSE, num_scenarios)
 )
 
 loop_over_input_params_fit_stan(input_params_container_only_no_sd)
@@ -53,7 +55,8 @@ input_params_all_no_sd = data.frame(
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = c(1000, 2000, 3000, 4000, 5000),
-  num_container_data = c(1000, 2000, 3000, 4000, 5000)
+  num_container_data = c(1000, 2000, 3000, 4000, 5000),
+  entry_random_effect = rep(FALSE, num_scenarios)
 )
 
 loop_over_input_params_fit_stan(input_params_all_no_sd)
