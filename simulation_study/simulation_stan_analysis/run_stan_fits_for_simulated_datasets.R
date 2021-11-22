1c6c0ab183345f4d15a34a5b09def5b0c29bc4a8
cmbaker00 <cmbaker00@gmail.com>
2021-11-23 09:27:06 +1100

    updated simulation analysis with entry size 1,5,10,20

--- simulation_study/simulation_stan_analysis/run_stan_fits_for_simulated_datasets.R
+++ simulation_study/simulation_stan_analysis/run_stan_fits_for_simulated_datasets.R
@@ -1,5 +1,8 @@
 source(file.path('simulation_study', 'simulation_stan_analysis', 'subset_simulated_data.R'))
 
+entry_size_list <- c(1,5,10,20)
+entry_size_min <- entry_size_list[1]
+entry_size_max <- entry_size_list[length(entry_size_list)]
 
 loop_over_input_params_fit_stan <- function (input_params) {
   # load and subset data
@@ -22,8 +25,8 @@ loop_over_input_params_fit_stan <- function (input_params) {
 ## Line data only, no SD, varying amount of data
 num_scenarios <- 5
 input_params_line_only_no_sd = data.frame(
-  min_entry_size = rep(2, num_scenarios),
-  max_entry_size = rep(5, num_scenarios),
+  min_entry_size = rep(entry_size_min, num_scenarios),
+  max_entry_size = rep(entry_size_max, num_scenarios),
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = c(1000, 2000, 3000, 4000, 5000),
@@ -37,8 +40,8 @@ loop_over_input_params_fit_stan(input_params_line_only_no_sd)
 ## Container data only, no SD, varying amount of data
 num_scenarios <- 5
 input_params_container_only_no_sd = data.frame(
-  min_entry_size = rep(2, num_scenarios),
-  max_entry_size = rep(5, num_scenarios),
+  min_entry_size = rep(entry_size_min, num_scenarios),
+  max_entry_size = rep(entry_size_min, num_scenarios),
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = rep(0, num_scenarios),
@@ -52,8 +55,8 @@ loop_over_input_params_fit_stan(input_params_container_only_no_sd)
 # Equal mix of container and line, no SD, varying amount of data
 num_scenarios <- 5
 input_params_all_no_sd = data.frame(
-  min_entry_size = rep(2, num_scenarios),
-  max_entry_size = rep(5, num_scenarios),
+  min_entry_size = rep(entry_size_min, num_scenarios),
+  max_entry_size = rep(entry_size_max, num_scenarios),
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = c(1000, 2000, 3000, 4000, 5000),
@@ -73,8 +76,8 @@ load_sim_data %>% group_by(Mode) %>% summarise(n())
 num_rows <- c(10000, 15000, 20000, 25000)
 num_scenarios <- length(num_rows)
 input_params_all_no_sd = data.frame(
-  min_entry_size = rep(2, num_scenarios),
-  max_entry_size = rep(5, num_scenarios),
+  min_entry_size = rep(entry_size_min, num_scenarios),
+  max_entry_size = rep(entry_size_max, num_scenarios),
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data = num_rows,
@@ -88,8 +91,8 @@ loop_over_input_params_fit_stan(input_params_all_no_sd)
 # Constant 1k rows of data, no SD, varying proportion of line/container data
 num_scenarios <- 9
 input_params_all_no_sd_change_combination = data.frame(
-  min_entry_size = rep(2, num_scenarios),
-  max_entry_size = rep(5, num_scenarios),
+  min_entry_size = rep(entry_size_min, num_scenarios),
+  max_entry_size = rep(entry_size_max, num_scenarios),
   sd = rep(0, num_scenarios),
   target_num_rows_data = rep('1e+05', num_scenarios),
   num_line_data =      c(900, 800, 700, 600, 500, 400, 300, 200, 100),
@@ -103,7 +106,7 @@ loop_over_input_params_fit_stan(input_params_all_no_sd_change_combination)
 
 
 # Chanign entry sizes, runnign with equal mix of line and container, and contaienr only. 500 to 2500 of each in steps of 500
-for (fixed_entry_size in 2:5){
+for (fixed_entry_size in entry_size_list){
 num_scenarios <- 10
 input_params_all_no_sd_change_combination = data.frame(
   min_entry_size = rep(fixed_entry_size, num_scenarios),
@@ -124,7 +127,7 @@ loop_over_input_params_fit_stan(input_params_all_no_sd_change_combination)
 
 
 # Chanign entry sizes, runnign with equal mix of line and container, and contaienr only. 500 to 2500 of each in steps of 500
-for (fixed_entry_size in 2:5){
+for (fixed_entry_size in entry_size_list){
   num_scenarios <- 5
   input_params_all_no_sd_change_combination = data.frame(
     min_entry_size = rep(fixed_entry_size, num_scenarios),
